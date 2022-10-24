e839a16d9e054651deeb2dbfc5f1d4553340910f
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-10-24 15:09:16 +0200

    added err helpers

---
+++ errors.go
@@ -0,0 +1,27 @@
+package just
+
+import "errors"
+
+// ErrIsAnyOf return true when at least one expression
+// `errors.Is(err, errSlice[N])` return true.
+func ErrIsAnyOf(err error, errSlice []error) bool {
+	for i := range errSlice {
+		if errors.Is(err, errSlice[i]) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// ErrIsNotAnyOf return true when all errors from errSlice is not
+// `errors.Is(err, errSlice[N])`.
+func ErrIsNotAnyOf(err error, errSlice []error) bool {
+	for i := range errSlice {
+		if errors.Is(err, errSlice[i]) {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ errors_test.go
@@ -0,0 +1,100 @@
+package just_test
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/kazhuravlev/just"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrIsAnyOf(t *testing.T) {
+	t.Parallel()
+
+	wrapped := func(e error) error { return fmt.Errorf("wrapped %w", e) }
+
+	table := []struct {
+		err      error
+		errSlice []error
+		exp      bool
+	}{
+		{
+			err:      nil,
+			errSlice: nil,
+			exp:      false,
+		},
+		{
+			err:      io.EOF,
+			errSlice: []error{io.EOF, io.ErrClosedPipe},
+			exp:      true,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: []error{io.EOF, io.ErrClosedPipe},
+			exp:      true,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: []error{io.ErrClosedPipe},
+			exp:      false,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: nil,
+			exp:      false,
+		},
+	}
+
+	for _, row := range table {
+		t.Run("", func(t *testing.T) {
+			res := just.ErrIsAnyOf(row.err, row.errSlice)
+			assert.Equal(t, row.exp, res)
+		})
+	}
+}
+
+func TestErrIsNotAnyOf(t *testing.T) {
+	t.Parallel()
+
+	wrapped := func(e error) error { return fmt.Errorf("wrapped %w", e) }
+
+	table := []struct {
+		err      error
+		errSlice []error
+		exp      bool
+	}{
+		{
+			err:      nil,
+			errSlice: nil,
+			exp:      true,
+		},
+		{
+			err:      io.EOF,
+			errSlice: []error{io.EOF, io.ErrClosedPipe},
+			exp:      false,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: []error{io.EOF, io.ErrClosedPipe},
+			exp:      false,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: []error{io.ErrClosedPipe},
+			exp:      true,
+		},
+		{
+			err:      wrapped(io.EOF),
+			errSlice: nil,
+			exp:      true,
+		},
+	}
+
+	for _, row := range table {
+		t.Run("", func(t *testing.T) {
+			res := just.ErrIsNotAnyOf(row.err, row.errSlice)
+			assert.Equal(t, row.exp, res)
+		})
+	}
+}
